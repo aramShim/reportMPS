c14ccde4e5b1286012ddec7312a2855742e81f3f
arams <oeo8909@naver.com>
2024-10-25 18:17:42 +0900

    page 추가 및 layout 작업

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,8 @@
 $(function(){
   leftMenu();
   toggleFavorite();
+  
+  if ($('.js-btn-mypage').length > 0) {btnMypage();}
 
   $('.mo-header-menu-btn').click(function(){
     $('body').toggleClass('no-scroll');
@@ -16,15 +18,20 @@ function toggleFavorite(){
   })
 }
 function leftMenu(){
-  $('.sub-menu').hide();
+  //$('.sub-menu').hide();
   $('.menu-label').click(function(e){
     if($(this).attr('href') == '#'){
       e.preventDefault();
-    }
-    
+    }    
     $(this).toggleClass('active');
     $(this).next('.sub-menu').slideToggle();
 
   })
+}
+function btnMypage(){
+	$('.js-btn-mypage').click(function(){
+		$(this).toggleClass('open');
+		$(this).next('.mypage-wrap').toggleClass('open');
+	})
 }
 
